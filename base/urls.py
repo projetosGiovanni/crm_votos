f74dc16f3702c63b16785b4f5d8b9a5b6b481f6f
Marcus Vinicius <mvcoosta@gmail.com>
2022-07-17 12:00:46 -0400

    .

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('cadastrar-grupo/', views.cadastrarGrupo, name='cadastrar-grupo'),
-    path('cadastrar-equipe/', views.cadastrarEquipe, name='cadastrar-equipe'),
+    path('cadastrar-colaborador/', views.cadastrarColaborador, name='cadastrar-colaborador'),
     #     path('cadastrar-líder/', views.cadastrarLíder, name='cadastrar-líder'),
     #     path('cadastrar-cabo/', views.cadastrarCabo, name='cadastrar-cabo'),
     #     path('cadastrar-eleitor/', views.cadastrarEleitor, name='cadastrar-eleitor'),
--- base/views.py
+++ base/views.py
@@ -5,20 +5,6 @@ from .models import Pessoa, Voto
 from .forms import PessoaForm, GrupoForm, EquipeForm, LíderForm, CaboForm, VotoForm
 
 
-def salvarNoBanco(request):
-    if request.method == 'POST':
-        pessoa_form = PessoaForm(request.POST)
-        pessoa = None
-
-        if pessoa_form.is_valid():
-            print("saving...")
-            pessoa = pessoa_form.save()
-            print(pessoa.pk)
-
-        form = EquipeForm(request.POST)
-        if form.is_valid():
-            form.save()
-
 # Create your views here.
 
 
@@ -31,14 +17,24 @@ def home(request):
 def cadastrarGrupo(request):
     form = GrupoForm()
     context = {'form': form}
-    return render(request, 'base/colaborador_form.html', context)
+    return render(request, 'base/grupo_form.html', context)
 
 
-def cadastrarEquipe(request):
-    hierarquia = "nova Equipe"
+def cadastrarColaborador(request):
     pessoa_form = PessoaForm()
     form = EquipeForm()
-    context = {'pessoa_form': pessoa_form, 'form': form, 'hierarquia': hierarquia}
+    context = {'pessoa_form': pessoa_form}
 
-    salvarNoBanco(request)
+    if request.method == 'POST':
+        pessoa_form = PessoaForm(request.POST)
+
+        if pessoa_form.is_valid():
+            print("saving...")
+            pessoa = pessoa_form.save()
+
+            hierarquia = request.POST['hierarquia']
+
+            if hierarquia == "cabo":
+                pass
+
     return render(request, 'base/colaborador_form.html', context)
