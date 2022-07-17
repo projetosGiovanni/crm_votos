d51ed5d6056e388a7f347864ad76a94f72226309
Marcus Vinicius <mvcoosta@gmail.com>
2022-07-17 15:25:15 -0400

    Refatorado view.py

--- base/views.py
+++ base/views.py
@@ -5,6 +5,35 @@ from .models import Pessoa, Grupo, Equipe, Líder, Cabo, Voto
 from .forms import PessoaForm, GrupoForm, EquipeForm, LíderForm, CaboForm, VotoForm
 
 
+# Função auxiliar
+
+
+def cadastrar(request, hierarquia_form, hierarquia, superior):
+    pessoa_form = PessoaForm(request.POST)
+    if pessoa_form.is_valid():
+        pessoa = pessoa_form.save()
+
+        if hierarquia_form.is_valid():
+            superior_pk = int(request.POST[superior])
+
+            if hierarquia == 'equipe':
+                grupo = Grupo.objects.get(pk=superior_pk)
+                equipe = Equipe(grupo=grupo, coordenador=pessoa)
+                equipe.save()
+            if hierarquia == 'líder':
+                equipe = Equipe.objects.get(pk=superior_pk)
+                líder = Líder(equipe=equipe, líder=pessoa)
+                líder.save()
+            if hierarquia == 'cabo':
+                líder = Líder.objects.get(pk=superior_pk)
+                cabo = Cabo(líder=líder, cabo=pessoa)
+                cabo.save()
+            if hierarquia == 'eleitor':
+                cabo = Cabo.objects.get(pk=superior_pk)
+                voto = Voto(cabo=cabo, eleitor=pessoa)
+                voto.save()
+            return redirect('home')
+
 # Create your views here.
 
 
@@ -22,87 +51,31 @@ def cadastrarGrupo(request):
 
 def cadastrarEquipe(request):
     if request.method == 'POST':
-        hierarquia_form = EquipeForm(request.POST)
-        pessoa_form = PessoaForm(request.POST)
-        if pessoa_form.is_valid():
-            pessoa = pessoa_form.save()
+        return cadastrar(request, EquipeForm(request.POST), 'equipe', 'grupo')
 
-            if hierarquia_form.is_valid():
-                grupo_pk = int(request.POST['grupo'])
-                grupo = Grupo.objects.get(pk=grupo_pk)
-                equipe = Equipe(grupo=grupo, coordenador=pessoa)
-                equipe.save()
-                return redirect('home')
-
-    hierarquia = 'coordenador'
-    pessoa_form = PessoaForm()
-    hierarquia_form = EquipeForm()
-
-    context = {'form': hierarquia_form, 'pessoa_form': pessoa_form, 'hierarquia': hierarquia}
+    context = {'form': EquipeForm(), 'pessoa_form': PessoaForm(), 'título': 'nova equipe'}
     return render(request, 'base/colaborador_form.html', context)
 
 
 def cadastrarLíder(request):
     if request.method == 'POST':
-        hierarquia_form = LíderForm(request.POST)
-        pessoa_form = PessoaForm(request.POST)
-        if pessoa_form.is_valid():
-            pessoa = pessoa_form.save()
+        return cadastrar(request, LíderForm(request.POST), 'líder', 'equipe')
 
-            if hierarquia_form.is_valid():
-                equipe_pk = int(request.POST['equipe'])
-                equipe = Equipe.objects.get(pk=equipe_pk)
-                equipe = Líder(equipe=equipe, líder=pessoa)
-                equipe.save()
-                return redirect('home')
-
-    hierarquia = 'líder'
-    pessoa_form = PessoaForm()
-    hierarquia_form = LíderForm()
-
-    context = {'form': hierarquia_form, 'pessoa_form': pessoa_form, 'hierarquia': hierarquia}
+    context = {'form': LíderForm(), 'pessoa_form': PessoaForm(), 'título': 'novo líder'}
     return render(request, 'base/colaborador_form.html', context)
 
 
 def cadastrarCabo(request):
     if request.method == 'POST':
-        hierarquia_form = CaboForm(request.POST)
-        pessoa_form = PessoaForm(request.POST)
-        if pessoa_form.is_valid():
-            pessoa = pessoa_form.save()
+        return cadastrar(request, CaboForm(request.POST), 'cabo', 'líder')
 
-            if hierarquia_form.is_valid():
-                líder_pk = int(request.POST['líder'])
-                líder = Líder.objects.get(pk=líder_pk)
-                líder = Cabo(líder=líder, cabo=pessoa)
-                líder.save()
-                return redirect('home')
-
-    hierarquia = 'cabo eleitoral'
-    pessoa_form = PessoaForm()
-    hierarquia_form = CaboForm()
-
-    context = {'form': hierarquia_form, 'pessoa_form': pessoa_form, 'hierarquia': hierarquia}
+    context = {'form': CaboForm(), 'pessoa_form': PessoaForm(), 'título': 'novo cabo eleitoral'}
     return render(request, 'base/colaborador_form.html', context)
 
 
 def cadastrarVoto(request):
     if request.method == 'POST':
-        hierarquia_form = VotoForm(request.POST)
-        pessoa_form = PessoaForm(request.POST)
-        if pessoa_form.is_valid():
-            pessoa = pessoa_form.save()
+        return cadastrar(request, VotoForm(request.POST), 'eleitor', 'cabo')
 
-            if hierarquia_form.is_valid():
-                cabo_pk = int(request.POST['cabo'])
-                cabo = Cabo.objects.get(pk=cabo_pk)
-                cabo = Voto(cabo=cabo, eleitor=pessoa)
-                cabo.save()
-                return redirect('home')
-
-    hierarquia = 'Eleitor'
-    pessoa_form = PessoaForm()
-    hierarquia_form = VotoForm()
-
-    context = {'form': hierarquia_form, 'pessoa_form': pessoa_form, 'hierarquia': hierarquia}
+    context = {'form': VotoForm(), 'pessoa_form': PessoaForm(), 'título': 'novo eleitor'}
     return render(request, 'base/colaborador_form.html', context)
